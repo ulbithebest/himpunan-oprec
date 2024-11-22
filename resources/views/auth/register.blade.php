4adbd6135341ac5f2d2ca69f9284bc0a41679cb8
rifkyadipura <rifkyadipura@gmail.com>
2024-11-22 21:46:37 +0700

    Perbaikan pada form register untuk validasi nomor telepon

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,10 +34,23 @@
 
             <!-- Nama -->
             <div class="mb-3">
-                <label for="name" class="form-label text-secondary">{{ __('Nama') }}</label>
+                <label for="name" class="form-label text-secondary">{{ __('Nama Lengkap') }}</label>
                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                 @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+
+            <!-- No. Telepon -->
+            <div class="mb-3">
+                <label for="phone" class="form-label text-secondary">{{ __('No. Telepon (WhatsApp)') }}</label>
+                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror"
+                       name="phone" value="{{ old('phone') }}" placeholder="Contoh: 08XXXXXXXXXX" required>
+
+                @error('phone')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
